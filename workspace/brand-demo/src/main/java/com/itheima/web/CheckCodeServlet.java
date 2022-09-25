0edc6d578461935e38cfa3a42a69524f7284dbe6
wgy9814 <3208886054@qq.com>
2022-09-25 22:45:33 +0800

    jsp完结

---
+++ workspace/brand-demo/src/main/java/com/itheima/web/CheckCodeServlet.java
@@ -0,0 +1,35 @@
+package com.itheima.web;
+
+import com.itheima.util.CheckCodeUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+
+@WebServlet("/checkCodeServlet")
+public class CheckCodeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // 生成验证码
+        ServletOutputStream os = response.getOutputStream();
+        String checkCode = CheckCodeUtil.outputVerifyImage(100, 50, os, 4);
+
+
+        // 存入Session
+        HttpSession session = request.getSession();
+        session.setAttribute("checkCodeGen",checkCode);
+
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        this.doGet(request, response);
+    }
+}
