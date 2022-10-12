1e8725c5af166970ba72c2d4faeeba7b55a48098
wgy9814 <3208886054@qq.com>
2022-10-12 14:20:32 +0800

    spring 事务 添加注释说明

--- ssm_project/spring/spring_25_case_transfer_log/src/main/java/com/itheima/config/MybatisConfig.java
+++ ssm_project/spring/spring_25_case_transfer_log/src/main/java/com/itheima/config/MybatisConfig.java
@@ -11,7 +11,14 @@ public class MybatisConfig {
     @Bean
     public SqlSessionFactoryBean sqlSessionFactory(DataSource dataSource){
         SqlSessionFactoryBean ssfb = new SqlSessionFactoryBean();
+        //设置模型类的别名扫描
         ssfb.setTypeAliasesPackage("com.itheima.domain");
+        //代替xml中的
+        //<typeAliases>
+        //<package name="com.itheima.domain" / >
+        //</typeAliases>
+        //设置数据源
+
         ssfb.setDataSource(dataSource);
         return ssfb;
     }
@@ -20,6 +27,10 @@ public class MybatisConfig {
     public MapperScannerConfigurer mapperScannerConfigurer(){
         MapperScannerConfigurer msc = new MapperScannerConfigurer();
         msc.setBasePackage("com.itheima.dao");
+        //代替xml中的
+        //<mappers>
+        //<package name="com.itheima.dao" / >
+        //</mappers>
         return msc;
     }
 }
