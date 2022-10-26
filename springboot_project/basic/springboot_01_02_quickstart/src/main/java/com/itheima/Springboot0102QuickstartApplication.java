3414438568d754e60c5fbd20f51270ed6e2c34f5
wgy9814 <3208886054@qq.com>
2022-10-26 21:44:15 +0800

    springboot_2

---
+++ springboot_project/basic/springboot_01_02_quickstart/src/main/java/com/itheima/Springboot0102QuickstartApplication.java
@@ -0,0 +1,15 @@
+package com.itheima;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Springboot0102QuickstartApplication {
+
+	public static void main(String[] args)   {
+		SpringApplication.run(Springboot0102QuickstartApplication.class, args);
+	}
+
+}
