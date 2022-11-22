86469e6095dd812aaff50d119d3a637fe02af192
wgy9814 <3208886054@qq.com>
2022-11-22 16:47:38 +0800

    设计模式 1

---
+++ design_patterns_project/design_patterns_01/src/main/java/com/itheima/principles/demo1/AbstractSkin.java
@@ -0,0 +1,12 @@
+package com.itheima.principles.demo1;
+
+/**
+ * @version v1.0
+ * @ClassName: AbstractSkin
+ * @Description: 抽象皮肤类
+ * @Author: 黑马程序员
+ */
+public abstract class AbstractSkin {
+
+    public abstract void display();
+}
